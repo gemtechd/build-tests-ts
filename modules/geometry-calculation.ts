9114272e1498553b3b620c8c8f32f70b77cfb5be
gemtechd <gemtech.d@gmail.com>
2025-07-10 07:45:22 +0300

    code review

--- modules/geometry-calculation.ts
+++ modules/geometry-calculation.ts
@@ -1,7 +1,7 @@
 import Line from './ecs6-class/line';
 import Point from './ecs6-class/point';
 
-const calculateDistance = (point1: Point, point2: Point): number => {
+export const calculateDistance = (point1: Point, point2: Point): number => {
     let distanceX = (point2.x - point1.x) ** 2;
     let distanceY = (point2.y - point2.y) ** 2;
     const distance = Math.sqrt(distanceX + distanceY);
@@ -18,17 +18,15 @@ export const calculateJunctionPoint = (line1: Line, line2: Line): Boolean | Poin
         }
     }
     else {
-        if (line1) {
-            if ( line1.n!==undefined&& line1.slope!==undefined && line2.n!==undefined&& line2.slope!==undefined) {
-                const x = (line1.n - line2.n) / (line2.slope - line1.slope)
-                const junctionPoint = line1.getPointByX(x);
-                return junctionPoint
-            }
+        if (line1.n !== undefined && line1.slope !== undefined && line2.n !== undefined && line2.slope !== undefined) {
+            const x = (line1.n - line2.n) / (line2.slope - line1.slope)
+            const junctionPoint = line1.getPointByX(x);
+            return junctionPoint
         }
     }
 }
 
-export const isPointOnLine = (line: Line, point: Point) :Boolean=> {
+export const isPointOnLine = (line: Line, point: Point): Boolean => {
     const proxyLine = new Line({ point1: line.point1, point2: point })
     proxyLine.calculateSlope()
     if (line.slope === proxyLine.slope) {
