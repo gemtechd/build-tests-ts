1468891979e3d534adc8a893a43c7f4836a6c714
chany waingort <tichnut28@8547900.org.il>
2025-07-14 12:13:48 +0300

    tests

--- modules/tests/point.test.ts
+++ modules/tests/point.test.ts
@@ -1,5 +1,5 @@
 import Point from "../ecs6-class/point";
-
+import Line from "../ecs6-class/line";
 // filepath: d:\chany waingort\build-tests-ts\modules\ecs6-class\point.test.ts
 
 describe("Point Class", () => {
@@ -26,4 +26,47 @@ describe("Point Class", () => {
         point.moveHorizontal(-2);
         expect(point.x).toBe(3); // 5 - 2
     });
+});
+
+
+
+
+
+
+test("Constructor initializes with default values", () => {
+    const line = new Line();
+    expect(line.point1).toEqual(new Point());
+    expect(line.point2).toEqual(new Point());
+    expect(line.slope).toBeUndefined();
+    expect(line.n).toBeUndefined();
+});
+
+test("Constructor initializes with custom values", () => {
+    const point1 = new Point({ x: 1, y: 2 });
+    const point2 = new Point({ x: 3, y: 4 });
+    const line = new Line({ point1, point2, slope: 2, n: 0 });
+    expect(line.point1).toEqual(point1);
+    expect(line.point2).toEqual(point2);
+    expect(line.slope).toBe(2);
+    expect(line.n).toBe(0);
+});
+
+test("getPointOnXAsis returns correct point", () => {
+    const point1 = new Point({ x: 1, y: 2 });
+    const point2 = new Point({ x: 3, y: 6 });
+    const line = new Line({ point1, point2 });
+    line.calculateSlope();
+    line.calculateNOfLineFunction();
+    const pointOnXAxis = line.getPointOnXAsis();
+    expect(pointOnXAxis).toEqual(new Point({ x: 0, y: 0 }));
+});
+
+test("getPointOnYAsis returns correct point", () => {
+    const point1 = new Point({ x: 1, y: 2 });
+    const point2 = new Point({ x: 3, y: 6 });
+    const line = new Line({ point1, point2 });
+    line.calculateSlope();
+    line.calculateNOfLineFunction();
+    const pointOnYAxis = line.getPointOnYAsis();
+    expect(pointOnYAxis).toEqual(new Point({ x: 0, y: 0 }));
 });
