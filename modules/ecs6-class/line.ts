339e099429ddff310f67a7624d5c2b8d01888851
brachy vacshtoc <tichnut1028547900.org.il>
2025-07-15 13:36:00 +0300

    push Change

--- modules/ecs6-class/line.ts
+++ modules/ecs6-class/line.ts
@@ -1,62 +1,63 @@
 import Point from "./point";
 
 export default class Line {
-    point1: Point
-    point2: Point
-    n: number | undefined
-    slope: number | undefined
+    point1: Point;
+    point2: Point;
+    n: number | undefined;
+    slope: number | undefined;
+
     constructor({ point1 = new Point(), point2 = new Point(), n = undefined, slope = undefined }: { point1?: Point, point2?: Point, n?: number, slope?: number } = {}) {
         this.point1 = point1;
         this.point2 = point2;
         this.slope = slope;
         this.n = n;
     }
-
-  
-
-    calculateSlope() {
-    const dx = this.point2.x - this.point1.x;
-    if (dx === 0) {
-        this.slope = undefined; 
-    } else {
-        this.slope = (this.point2.y - this.point1.y) / dx;
+    setPoints(p1: Point, p2: Point) {
+        this.point1 = p1;
+        this.point2 = p2;
     }
-    return this.slope;
-}
+    calculateSlope() {
+        const dx = this.point2.x - this.point1.x;
+        if (dx === 0) {
+            this.slope = undefined; 
+        } else {
+            this.slope = (this.point2.y - this.point1.y) / dx;
+        }
+        return this.slope;
+    }
 
     calculateNOfLineFunction() {
         this.calculateSlope();
-    if (this.slope !== undefined)
-        this.n = this.point1.y - this.slope * this.point1.x
-}
+        if (this.slope !== undefined) {
+            this.n = this.point1.y - this.slope * this.point1.x;
+        } else {
+            this.n = undefined; 
+        }
+    }
 
     getPointOnXAsis() {
         this.calculateNOfLineFunction();
-        return this.getPointByY(0)
+        return this.getPointByY(0);
     }
 
     getPointOnYAsis() {
         this.calculateNOfLineFunction();
-        return this.getPointByX(0)
+        return this.getPointByX(0);
     }
 
-
     getPointByX(x: number) {
-    if (this.slope === undefined) this.calculateSlope();
-    if (this.n === undefined) this.calculateNOfLineFunction();
-    if (this.slope === undefined || this.n === undefined) return undefined;
-    const y = this.slope * x + this.n;
-    return new Point({ x, y });
-}
+        if (this.slope === undefined) this.calculateSlope();
+        if (this.n === undefined) this.calculateNOfLineFunction();
+        if (this.slope === undefined || this.n === undefined) return undefined;
+        const y = this.slope * x + this.n;
+        return new Point({ x, y });
+    }
 
     getPointByY(y: number) {
-    if (this.slope === undefined) this.calculateSlope();
-    if (this.n === undefined) this.calculateNOfLineFunction();
-    if (this.slope === 0) return undefined; 
-    if (this.slope === undefined || this.n === undefined) return undefined;
-    const x = (y - this.n) / this.slope;
-    return new Point({ x, y });
-}
-
-
+        if (this.slope === undefined) this.calculateSlope();
+        if (this.n === undefined) this.calculateNOfLineFunction();
+        if (this.slope === undefined || this.n === undefined) return undefined;
+        const x = (y - this.n) / this.slope;
+        return new Point({ x, y });
+    }
 }
--- test/line.test.ts
+++ test/line.test.ts
@@ -1,7 +1,78 @@
 import Line from '../modules/ecs6-class/line';
 import Point from '../modules/ecs6-class/point';
 
+jest.mock('../modules/ecs6-class/line', () => {
+    return jest.fn().mockImplementation(() => {
+        const instance = {
+            point1: new Point({ x: 0, y: 0 }),
+            point2: new Point({ x: 2, y: 2 }),
+            slope: undefined,
+            n: undefined,
+            setPoints: jest.fn(function (p1: Point, p2: Point) {
+                this.point1 = p1;
+                this.point2 = p2;
+            }),
+            calculateSlope: jest.fn(function () {
+                const dx = this.point2.x - this.point1.x;
+                if (dx === 0) {
+                    this.slope = undefined;
+                } else {
+                    this.slope = (this.point2.y - this.point1.y) / dx;
+                }
+                return this.slope;
+            }),
+            calculateNOfLineFunction: jest.fn(function () {
+                if (this.slope === undefined) {
+                    this.calculateSlope();
+                }
+                if (this.slope !== undefined) {
+                    this.n = this.point1.y - this.slope * this.point1.x;
+                }
+            }),
+            getPointByX: jest.fn(function (x) {
+                if (this.slope === undefined) this.calculateSlope();
+                if (this.n === undefined) this.calculateNOfLineFunction();
+                if (this.slope === undefined || this.n === undefined) return undefined;
+                const y = this.slope * x + this.n;
+                return new Point({ x, y });
+            }),
+            getPointByY: jest.fn(function (y) {
+                if (this.slope === undefined) this.calculateSlope();
+                if (this.n === undefined) this.calculateNOfLineFunction();
+                if (this.slope === undefined || this.n === undefined) return undefined;
+                const x = (y - this.n) / this.slope;
+                return new Point({ x, y });
+            }),
+            getPointOnXAsis: jest.fn(function () {
+                this.calculateNOfLineFunction();
+                return this.getPointByY(0);
+            }),
+            getPointOnYAsis: jest.fn(function () {
+                this.calculateNOfLineFunction();
+                return this.getPointByX(0);
+            }),
+        };
+        return instance;
+    });
+});
+
 describe('Line', () => {
+    let mockLine: Line;
+    beforeEach(() => {
+        mockLine = new Line();
+        mockLine.setPoints(new Point({ x: 0, y: 0 }), new Point({ x: 2, y: 2 }));
+    });
+
+    it('should mock calculateSlope', () => {
+        const slope = mockLine.calculateSlope();
+        expect(slope).toBe(1);
+    });
+
+    it('should mock calculateNOfLineFunction', () => {
+        mockLine.calculateNOfLineFunction();
+        expect(mockLine.n).toBe(0);
+    });
+
     it('should create a line with two points', () => {
         const p1 = new Point({ x: 0, y: 0 });
         const p2 = new Point({ x: 2, y: 2 });
